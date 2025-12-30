4fa43674f902a02a08bf793a23fca913e1c386f4
coder7475 <robiulhossain7475@gmail.com>
2025-12-30 14:32:50 +0600

    learn: playwright test hooks

--- playwright/first_look_js/tests/elementix/first.spec.js
+++ playwright/first_look_js/tests/elementix/first.spec.js
@@ -1,29 +1,27 @@
 import { test, expect } from "@playwright/test";
 
-test("has title", async ({ page }) => {
-  test.setTimeout(60_000);
+test.describe("Test Suite 1", () => {
+  test.beforeEach(async ({ page }) => {
+    // Go to the starting url before each test.
+    test.setTimeout(60_000);
 
-  await page.goto("https://dev.elementix.us/", {
-    waitUntil: "domcontentloaded",
-    timeout: 50_000,
+    await page.goto("https://dev.elementix.us/", {
+      waitUntil: "domcontentloaded",
+      timeout: 50_000,
+    });
   });
 
-  await expect(page).toHaveTitle(/Elementix Software Ventures LLC/);
-});
-
-test("has the elementix logo on navbar", async ({ page }) => {
-  test.setTimeout(60_000);
-
-  await page.goto("https://dev.elementix.us/", {
-    waitUntil: "domcontentloaded",
-    timeout: 50_000,
+  test("has title", async ({ page }) => {
+    await expect(page).toHaveTitle(/Elementix Software Ventures LLC/);
   });
 
-  await page.getByRole("link", { name: "Go to homepage" }).click();
+  test("has the elementix logo on navbar", async ({ page }) => {
+    await page.getByRole("link", { name: "Go to homepage" }).click();
 
-  const homeNavLink = page.locator("header").getByRole("link", {
-    name: "Home",
-    exact: true,
+    const homeNavLink = page.locator("header").getByRole("link", {
+      name: "Home",
+      exact: true,
+    });
+    await expect(homeNavLink).toBeVisible();
   });
-  await expect(homeNavLink).toBeVisible();
 });
